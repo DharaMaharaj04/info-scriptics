0b2bd8ace9139e0f71280428a6629510e671d58b
DHARA MAHARAJ <DharaMaharaj04>
2023-02-17 13:28:02 +0530

    serv-link-decor

--- src/pages/Retail.js
+++ src/pages/Retail.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Helmet } from 'react-helmet';
+
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
